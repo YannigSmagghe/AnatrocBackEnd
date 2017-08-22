fbd35e197f09d139e34ac58856adfc22cf1f3d17
alexandre <ayolia18@hotmail.fr>
2017-08-22 10:36:11 +0200

    fix input morzilla

--- app/assets/js/traitementjson.js
+++ app/assets/js/traitementjson.js
@@ -1,23 +1,5 @@
 //Traitement Json
 
-$.ajax({
-    url : 'http://localhost:8080',
-    type : 'GET',
-    dataType : 'JSON',
-    success : function(data){
-
-
-        console.log(data + 'succes');
-
-    },
-
-    error : function(data){
-        console.log(data + 'erreur');
-    },
-
-
-});
-
 function displayWeather(response) {
 
     $(".temps").hide();
@@ -61,8 +43,8 @@ $.getJSON( "result.json", function( data ) {
     // console.log(data.data[0].data.temperature);
     //console.log(data.data[0].data.temps);
     displayFromResponse(data.data);
-    displayTransport(data.data);
-    recupLocation(data.data);
+    displayTransport(data);
+   // recupLocation(data.data);
     $('.result_type').text(data.data[0].type);
 
 
