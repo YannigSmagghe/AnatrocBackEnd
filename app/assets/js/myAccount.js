8ca9f35ba2fafd8e07e30d7c4bf3a5be949e74c3
YannigSmagghe <smagghe.yannig@gmail.com>
2017-08-24 16:11:20 +0200

    menu change

--- app/assets/js/myAccount.js
+++ app/assets/js/myAccount.js
@@ -27,9 +27,24 @@ $.getJSON( "myaccount.json", function( data ) {
     })​
 });
 
+
+/** SET AUTH COOKIE **/
+// $.cookie("test", 1, {
+//     expires : 10,           //expires in 10 days
+//
+//     path    : '/',          //The value of the path attribute of the cookie
+//                             //(default: path of page that created the cookie).
+//
+//     domain  : 'jquery.com',  //The value of the domain attribute of the cookie
+//     //(default: domain of page that created the cookie).
+//
+//     secure  : true          //If set to true the secure attribute of the cookie
+//                             //will be set and the cookie transmission will
+//                             //require a secure protocol (defaults to false).
+// });
+
 /** Add address when u click on button**/
 $(document).on('click', '.address-btn', function() {
-
     if ($('#search-input-from').val() !== ''){
         $('#search-input-to').val(($(this).data('address')));
         showResultsPage();
