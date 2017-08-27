92e803db51bda55cdc57bb3847ec6d34aacebd50
Alexis Bussières <bussieres.alexis.dev@gmail.com>
2017-08-27 11:05:38 +0200

    Google OAuth work front + back and apikey to auth user (store in cookie)

--- app/assets/js/myAccount.js
+++ app/assets/js/myAccount.js
@@ -22,44 +22,47 @@ $.getJSON("myaccount.json", function (data) {
             $('.btn-job').append("<div class='col-lg-2 col-md-2 col-sm-2'><button type='button' title='" + item.job["start_address_name"] + "'  class='col-lg-3 col-md-3 col-sm-3 btn btn-primary address-btn' data-address='" + item.job["start_address_name"] + "'> <i class='fa fa-briefcase' aria-hidden='true'></i></button> </div><div class='col-lg-9 col-md-9 col-sm-6  col-sm-offset-3 col-md-offset-0 col-lg-offset-0'> my job</div>")
         }
 
-        $.each(item.favorite, function (keyAddress, addressName) {
-            var item = 'favorite-btn'+count;
-            if (count % 2 === 0){
-                $('.favorite-btn').append('<div  class="row" id='+count+'>');
-                //Create item
-                $('#'+count).append('<div class="btn-favorite btn-favorite-'+count+' col-lg-6 col-md-6 col-sm-6">');
-                $(".btn-favorite-"+count).append("<div class='col-lg-2 col-md-2 col-sm-2'><button type='button' title='" + addressName + "'  class='col-lg-3 col-md-3 col-sm-3  btn btn-primary btn-favorite address-btn' data-address='" + addressName + "'> <i class='fa fa-star' aria-hidden='true'></i></button> </div><div class='col-lg-9 col-md-9 col-sm-6 col-sm-offset-3 col-md-offset-0 col-lg-offset-0'> favorite address"+count+"</div></div>")
-            }else{
-                var countBefore = count -1;
-                item = 'favorite-btn'+countBefore;
-                $('#'+countBefore).append('<div class="btn-favorite btn-favorite-'+count+' col-lg-6 col-md-6 col-sm-6">');
-                $(".btn-favorite-"+count).append("<div class='col-lg-2 col-md-2 col-sm-2'><button type='button' title='" + addressName + "'  class='col-lg-3 col-md-3 col-sm-3 btn btn-primary btn-favorite address-btn' data-address='" + addressName + "'> <i class='fa fa-star' aria-hidden='true'></i></button></div><div class='col-lg-9 col-md-9 col-sm-6  col-sm-offset-3 col-md-offset-0 col-lg-offset-0'> favorite address"+count+"</div></div>")
-                //end row
-                $(item).append('</div>');
-            }
-            if (count % 2 !== 0){
+function onSignIn(googleUser) {
+    var id_token = googleUser.getAuthResponse().id_token;
+    var xhr = new XMLHttpRequest();
 
-            }
-            count++;
-        })​
-    })​
-});
+    xhr.open('POST', App.baseUri+'/login');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.onload = function() {
+        var json = JSON.parse(xhr.responseText);
 
+        createCookieAuthToken(json.data.token);
+    };
+    xhr.send('token=' + id_token+'&email='+googleUser.getBasicProfile().getEmail());
+}
 
-/** SET AUTH COOKIE **/
-// $.cookie("test", 1, {
-//     expires : 10,           //expires in 10 days
-//
-//     path    : '/',          //The value of the path attribute of the cookie
-//                             //(default: path of page that created the cookie).
-//
-//     domain  : 'jquery.com',  //The value of the domain attribute of the cookie
-//     //(default: domain of page that created the cookie).
-//
-//     secure  : true          //If set to true the secure attribute of the cookie
-//                             //will be set and the cookie transmission will
-//                             //require a secure protocol (defaults to false).
-// });
+const AUTH_COOKIE_NAME = 'anatroc.auth.token';
+const USER_INFO_KEY = 'user.info';
+
+function createCookieAuthToken(token) {
+    var d = new Date();
+
+    d.setTime(d.getTime() + (14*24*60*60*1000));
+    var expires = "expires="+ d.toUTCString();
+    document.cookie = AUTH_COOKIE_NAME + "=" + token + ";" + expires + ";path=/";
+}
+
+function getCookieAuthToken() {
+    var name = AUTH_COOKIE_NAME + "=";
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var ca = decodedCookie.split(';');
+
+    for(var i = 0; i <ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
 
 /** Add address when u click on button**/
 $(document).on('click', '.address-btn', function () {
